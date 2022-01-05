d1c8c312e6b00e4cc00a5bd678bc11722cc55027
ArtemA <ar.ar7emy@yandex.ru>
2022-01-05 13:36:30 +0300

    оптимизация шрифта

--- source/js/main.js
+++ source/js/main.js
@@ -1,31 +1,32 @@
-let navMain = document.querySelector('.main-nav');
-let navToggle = document.querySelector('.main-nav__toggle');
-const btnBefore = document.querySelector('.buttons__button--before');
-const btnAfter = document.querySelector('.buttons__button--after');
-const sliderImgBefore = document.querySelector('.examle__image-before');
-const sliderImgAfter = document.querySelector('.examle__image-after');
-const scale = document.querySelector('.buttons__scale-button');
+document.addEventListener("DOMContentLoaded", () => {
+  let navMain = document.querySelector('.main-nav');
+  let navToggle = document.querySelector('.main-nav__toggle');
+  const btnBefore = document.querySelector('.buttons__button--before');
+  const btnAfter = document.querySelector('.buttons__button--after');
+  const sliderImgBefore = document.querySelector('.examle__image-before');
+  const sliderImgAfter = document.querySelector('.examle__image-after');
 
-navMain.classList.remove('main-nav--nojs');
+  navMain.classList.remove('main-nav--nojs');
 
-navToggle.addEventListener('click', function() {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
+  navToggle.addEventListener('click', function() {
+    if (navMain.classList.contains('main-nav--closed')) {
+      navMain.classList.remove('main-nav--closed');
+      navMain.classList.add('main-nav--opened');
     } else {
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
-  }
+      navMain.classList.add('main-nav--closed');
+      navMain.classList.remove('main-nav--opened');
+    }
+  });
+
+  btnBefore.addEventListener('click', function(e) {
+    e.preventDefault();
+    sliderImgBefore.style.display = 'block';
+    sliderImgAfter.style.display = 'none';
+  });
+
+  btnAfter.addEventListener('click', function(e) {
+    e.preventDefault();
+    sliderImgBefore.style.display = 'none';
+    sliderImgAfter.style.display = 'block';
+  });
 });
-
-btnBefore.addEventListener('click', function(e) {
-  e.preventDefault();
-  sliderImgBefore.style.display = 'block';
-  sliderImgAfter.style.display = 'none';
-})
-
-btnAfter.addEventListener('click', function(e) {
-  e.preventDefault();
-  sliderImgBefore.style.display = 'none';
-  sliderImgAfter.style.display = 'block';
-})
