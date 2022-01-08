de0f36700f5ca8a465861d5ab8e544116384bc34
ArtemA <ar.ar7emy@yandex.ru>
2022-01-08 17:18:39 +0300

    слайдер

--- source/js/main.js
+++ source/js/main.js
@@ -5,6 +5,8 @@ document.addEventListener("DOMContentLoaded", () => {
   const btnAfter = document.querySelector('.buttons__button--after');
   const sliderImgBefore = document.querySelector('.examle__image-before');
   const sliderImgAfter = document.querySelector('.examle__image-after');
+  const sliderImgBeforeAfter = document.querySelector('.examle__image-before-after');
+  const slider = document.querySelector('.example__slider');
 
   navMain.classList.remove('main-nav--nojs');
 
@@ -21,12 +23,41 @@ document.addEventListener("DOMContentLoaded", () => {
   btnBefore.addEventListener('click', function(e) {
     e.preventDefault();
     sliderImgBefore.style.display = 'block';
+    sliderImgBeforeAfter.style.display = 'none';
     sliderImgAfter.style.display = 'none';
+    if(window.innerWidth < 768) {
+      slider.value = '6';
+    } else {
+      slider.value = '1';
+    }
   });
 
   btnAfter.addEventListener('click', function(e) {
     e.preventDefault();
     sliderImgBefore.style.display = 'none';
+    sliderImgBeforeAfter.style.display = 'none';
     sliderImgAfter.style.display = 'block';
+    if(window.innerWidth < 768) {
+      slider.value = '94';
+    } else {
+      slider.value = '100';
+    }
   });
+
+  slider.oninput = function() {
+    if(this.value < 50) {
+      sliderImgBefore.style.display = 'block';
+      sliderImgBeforeAfter.style.display = 'none';
+      sliderImgAfter.style.display = 'none';
+    } else if(this.value == 50 && window.innerWidth >= 768) {
+      sliderImgBefore.style.display = 'none';
+      sliderImgAfter.style.display = 'none';
+      sliderImgBeforeAfter.style.display = 'block';
+    }
+    else if(this.value > 50) {
+      sliderImgBefore.style.display = 'none';
+      sliderImgBeforeAfter.style.display = 'none';
+      sliderImgAfter.style.display = 'block';
+    }
+  }
 });
